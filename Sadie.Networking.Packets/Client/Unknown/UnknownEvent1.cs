0f1c7efd0c8c1b5e04db1ac504fbd048f6e29489
sam-habbo <habtard@gmail.com>
2022-02-02 15:27:56 +0000

    Documented unknown packet events

--- Sadie.Networking.Packets/Client/Unknown/UnknownEvent1.cs
+++ Sadie.Networking.Packets/Client/Unknown/UnknownEvent1.cs
@@ -5,6 +5,8 @@ namespace Sadie.Networking.Packets.Client.Unknown;
 
 public class UnknownEvent1 : INetworkPacketEvent
 {
+    // https://github.com/Domexx/SWF-Source-Clean-MS/blob/6a1d87045dcc8712ad58a9b1329724cbfd4106bc/src/com/sulake/habbo/communication/messages/outgoing/inventory/badges/_Str_9250.as
+    
     public async Task HandleAsync(INetworkClient client, INetworkPacketReader reader)
     {
         await client.WriteToStreamAsync(new PlayerIgnoredUsersWriter().GetAllBytes());
