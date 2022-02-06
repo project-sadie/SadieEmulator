7e50fa6e77a0662b4cfb557b3bfcd9de00090181
sam-habbo <habtard@gmail.com>
2022-02-06 23:49:37 +0000

    Cleaning up packets

--- Sadie.Networking.Packets/Client/Navigator/NavigatorDataEvent.cs
+++ Sadie.Networking.Packets/Client/Navigator/NavigatorDataEvent.cs
@@ -1,4 +1,5 @@
-﻿using Sadie.Networking.Client;
+﻿using Sadie.Game.Rooms;
+using Sadie.Networking.Client;
 using Sadie.Networking.Packets.Server.Navigator;
 using Sadie.Shared;
 
@@ -9,10 +10,8 @@ public class NavigatorDataEvent : INetworkPacketEvent
     public async Task HandleAsync(INetworkClient client, INetworkPacketReader reader)
     {
         await client.WriteToStreamAsync(new NavigatorMetaDataWriter().GetAllBytes());
-        await client.WriteToStreamAsync(new NavigatorLiftedRoomsWriter().GetAllBytes());
-        
+        await client.WriteToStreamAsync(new NavigatorLiftedRoomsWriter(new List<RoomEntity>()).GetAllBytes());
         await client.WriteToStreamAsync(new NavigatorCollapsedCategoriesWriter(SadieConstants.NavigatorCategories).GetAllBytes());
-        
         await client.WriteToStreamAsync(new NavigatorSavedSearchesWriter().GetAllBytes());
         await client.WriteToStreamAsync(new NavigatorEventCategoriesWriter().GetAllBytes());
     }
--- Sadie.Networking.Packets/Server/Navigator/NavigatorLiftedRoomsWriter.cs
+++ Sadie.Networking.Packets/Server/Navigator/NavigatorLiftedRoomsWriter.cs
@@ -1,9 +1,19 @@
+using Sadie.Game.Rooms;
+
 namespace Sadie.Networking.Packets.Server.Navigator;
 
 internal class NavigatorLiftedRoomsWriter : NetworkPacketWriter
 {
-    internal NavigatorLiftedRoomsWriter() : base(ServerPacketId.NavigatorLiftedRooms)
+    internal NavigatorLiftedRoomsWriter(List<RoomEntity> rooms) : base(ServerPacketId.NavigatorLiftedRooms)
     {
-        WriteInt(0);
+        WriteInt(rooms.Count);
+
+        foreach (var room in rooms)
+        {
+            WriteLong(room.Id);
+            WriteInt(0); // unknown
+            WriteString(""); // thumbnail?
+            WriteString(room.Name);
+        }
     }
 }
