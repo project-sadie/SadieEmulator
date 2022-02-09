e4257b85242d9134b0c18f50bd194c62df9f5031
sam-habbo <habtard@gmail.com>
2022-02-09 14:01:21 +0000

    Cleanup

--- Sadie.Networking.Packets/Client/ClientPacketId.cs
+++ Sadie.Networking.Packets/Client/ClientPacketId.cs
@@ -1,34 +1,33 @@
-namespace Sadie.Networking.Packets.Client
+namespace Sadie.Networking.Packets.Client;
+
+public class ClientPacketId
 {
-    public class ClientPacketId
-    {
-        public const short ClientVersion = 4000;
-        public const short ClientVariables = 1053;
-        public const short MachineId = 2490;
-        public const short SecureLogin = 2419;
-        public const short PerformanceLog = 3230;
-        public const short PlayerActivity = 3457;
-        public const short PlayerData = 357;
-        public const short PlayerBalance = 273;
-        public const short PlayerClubMembership = 3166;
-        public const short NavigatorData = 2110;
-        public const short PlayerFriendsList = 1523;
-        public const short PlayerMessengerInit = 2781;
-        public const short PlayerPing = 295;
-        public const short HotelViewData = 2912;
-        public const short PlayerUsername = 3878;
-        public const short PlayerMeMenuSettings = 2388;
-        public const short HotelViewBonusRare = 957;
-        public const short UnknownEvent1 = 1371;
-        public const short GameCenterRequestGames = 741;
-        public const short PromotedRooms = 2908;
-        public const short RoomCategories = 3027;
-        public const short NavigatorEventCategories = 1782;
-        public const short PlayerFriendRequestsList = 2448;
-        public const short PlayerSanctionStatus = 2746;
-        public const short UnknownEvent2 = 2487;
-        public const short RoomLoaded = 2312;
-        public const short UnknownEvent3 = 2519;
-        public const short RoomHeightmap = 3898;
-    }
+    public const short ClientVersion = 4000;
+    public const short ClientVariables = 1053;
+    public const short MachineId = 2490;
+    public const short SecureLogin = 2419;
+    public const short PerformanceLog = 3230;
+    public const short PlayerActivity = 3457;
+    public const short PlayerData = 357;
+    public const short PlayerBalance = 273;
+    public const short PlayerClubMembership = 3166;
+    public const short NavigatorData = 2110;
+    public const short PlayerFriendsList = 1523;
+    public const short PlayerMessengerInit = 2781;
+    public const short PlayerPing = 295;
+    public const short HotelViewData = 2912;
+    public const short PlayerUsername = 3878;
+    public const short PlayerMeMenuSettings = 2388;
+    public const short HotelViewBonusRare = 957;
+    public const short UnknownEvent1 = 1371;
+    public const short GameCenterRequestGames = 741;
+    public const short PromotedRooms = 2908;
+    public const short RoomCategories = 3027;
+    public const short NavigatorEventCategories = 1782;
+    public const short PlayerFriendRequestsList = 2448;
+    public const short PlayerSanctionStatus = 2746;
+    public const short UnknownEvent2 = 2487;
+    public const short RoomLoaded = 2312;
+    public const short UnknownEvent3 = 2519;
+    public const short RoomHeightmap = 3898;
 }
