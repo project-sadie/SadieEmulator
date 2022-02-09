783ae89ca5410b6493bcb72f4cddb15dec61e163
sam-habbo <habtard@gmail.com>
2022-02-09 20:12:14 +0000

    Forgot to push writer

---
+++ Sadie.Networking.Packets/Server/Players/Other/PlayerAchievementScoreWriter.cs
@@ -0,0 +1,9 @@
+﻿namespace Sadie.Networking.Packets.Server.Players.Other;
+
+internal class PlayerAchievementScoreWriter : NetworkPacketWriter
+{
+    internal PlayerAchievementScoreWriter(long achievementScore) : base(ServerPacketId.PlayerAchievementScore)
+    {
+        WriteLong(achievementScore);
+    }
+}
