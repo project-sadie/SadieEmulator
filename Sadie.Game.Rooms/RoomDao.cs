9b841d6d2bf722f0cad9540e77e8e9a0e337ad8d
sam-habbo <habtard@gmail.com>
2022-02-12 20:12:32 +0000

    Improvements to rooms

--- Sadie.Game.Rooms/RoomDao.cs
+++ Sadie.Game.Rooms/RoomDao.cs
@@ -10,7 +10,19 @@ public class RoomDao : BaseDao, IRoomDao
 
     public async Task<Tuple<bool, RoomEntity?>> TryGetRoomById(long roomId)
     {
-        var reader = await GetReaderAsync("SELECT `rooms`.`id`, `rooms`.`name`, `rooms`.`layout_id`, `room_layouts`.`name` AS `layout_name`, `room_layouts`.`heightmap`  FROM `rooms` INNER JOIN `room_layouts` ON `room_layouts`.`id` = `rooms`.`layout_id` WHERE `rooms`.`id` = @roomId LIMIT 1;", new Dictionary<string, object>
+        var reader = await GetReaderAsync(@"
+            SELECT 
+                   `rooms`.`id`, 
+                   `rooms`.`name`, 
+                   `rooms`.`layout_id`, 
+                   `room_layouts`.`name` AS `layout_name`, 
+                   `room_layouts`.`heightmap`,
+                   `room_layouts`.`door_x`,
+                   `room_layouts`.`door_y`,
+                   `room_layouts`.`door_z`
+            FROM `rooms` 
+                INNER JOIN `room_layouts` ON `room_layouts`.`id` = `rooms`.`layout_id` WHERE `rooms`.`id` = @roomId 
+            LIMIT 1;", new Dictionary<string, object>
         {
             { "roomId", roomId }
         });
