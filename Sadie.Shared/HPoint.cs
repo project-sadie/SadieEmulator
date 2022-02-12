035ecfa0eead079b6dd4030400817ff9a0562789
sam-habbo <habtard@gmail.com>
2022-02-12 20:12:55 +0000

    Create HPoint.cs

---
+++ Sadie.Shared/HPoint.cs
@@ -0,0 +1,15 @@
+﻿namespace Sadie.Shared;
+
+public class HPoint
+{
+    public int X { get; }
+    public int Y { get; }
+    public int Z { get; }
+    
+    public HPoint(int x, int y, int z)
+    {
+        X = x;
+        Y = y;
+        Z = z;
+    }
+}
