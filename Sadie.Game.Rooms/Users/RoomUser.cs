71d930e1bd451d49111870bcd2d8c3aa772ceec5
sam-habbo <habtard@gmail.com>
2022-02-13 03:05:57 +0000

    Update RoomUser.cs

--- Sadie.Game.Rooms/Users/RoomUser.cs
+++ Sadie.Game.Rooms/Users/RoomUser.cs
@@ -1,6 +1,15 @@
-﻿namespace Sadie.Game.Rooms.Users;
+﻿using Sadie.Shared;
+
+namespace Sadie.Game.Rooms.Users;
 
 public class RoomUser
 {
+    public int Id { get; }
+    public HPoint Point { get; }
     
+    public RoomUser(int id, HPoint point)
+    {
+        Id = id;
+        Point = point;
+    }
 }
