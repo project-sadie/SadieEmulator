2356ed05890a4ed27f9fcea59ae29479cb6b9ec7
sam-habbo <habtard@gmail.com>
2022-02-14 14:38:11 +0000

    Update PlayerChatMessageEvent.cs

--- Sadie.Networking.Packets/Server/Players/Chat/PlayerChatMessageEvent.cs
+++ Sadie.Networking.Packets/Server/Players/Chat/PlayerChatMessageEvent.cs
@@ -7,6 +7,12 @@ public class PlayerChatMessageEvent : INetworkPacketEvent
     public Task HandleAsync(INetworkClient client, INetworkPacketReader reader)
     {
         var message = reader.ReadString();
+
+        if (message.Length > 100)
+        {
+            return Task.CompletedTask;
+        }
+        
         var bubbleColor = reader.ReadInt();
         
         return Task.CompletedTask;
