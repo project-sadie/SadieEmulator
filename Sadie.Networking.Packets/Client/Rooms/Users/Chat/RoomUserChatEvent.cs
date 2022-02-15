022781b8b95d386e99588cde73e755e526d5c972
sam-habbo <habtard@gmail.com>
2022-02-15 17:53:33 +0000

    More improvements to rooms

--- Sadie.Networking.Packets/Client/Rooms/Users/Chat/RoomUserChatEvent.cs
+++ Sadie.Networking.Packets/Client/Rooms/Users/Chat/RoomUserChatEvent.cs
@@ -22,17 +22,14 @@ public class RoomUserChatEvent : INetworkPacketEvent
             return;
         }
 
-        var roomId = client.Player.LastRoomLoaded;
-        var (result, room) = _roomRepository.TryGetRoomById(roomId);
-
-        if (!result || room == null || !room.UserRepository.TryGet(client.Player.Id, out var user) || user == null)
+        if (!PacketHelpers.TryResolveRoomObjectsForClient(_roomRepository, client, out var room, out var roomUser))
         {
             return;
         }
-
-        var bubbleColor = reader.ReadInt();
-        var message = new RoomChatMessage(user, text, room, bubbleColor, 1);
         
-        await room.UserRepository.BroadcastDataToUsersAsync(new RoomUserChatWriter(message).GetAllBytes());
+        var bubbleColor = reader.ReadInt();
+        var message = new RoomChatMessage(roomUser!, text, room!, bubbleColor, 1);
+        
+        await room!.UserRepository.BroadcastDataToUsersAsync(new RoomUserChatWriter(message).GetAllBytes());
     }
 }
---
+++ Sadie.Networking.Packets/PacketHelpers.cs
@@ -0,0 +1,33 @@
+﻿using Sadie.Game.Rooms;
+using Sadie.Game.Rooms.Users;
+using Sadie.Networking.Client;
+
+namespace Sadie.Networking.Packets;
+
+public class PacketHelpers
+{
+    public static bool TryResolveRoomObjectsForClient(IRoomRepository roomRepository, INetworkClient client, out Room? room, out RoomUser? user)
+    {
+        var roomId = client.Player.LastRoomLoaded;
+        var (result, roomObject) = roomRepository.TryGetRoomById(roomId);
+
+        if (!result || roomObject == null)
+        {
+            room = null;
+            user = null;
+            
+            return false;
+        }
+
+        if (!roomObject.UserRepository.TryGet(client.Player.Id, out user))
+        {
+            room = null;
+            user = null;
+            
+            return false;
+        }
+
+        room = roomObject;
+        return true;
+    }
+}
