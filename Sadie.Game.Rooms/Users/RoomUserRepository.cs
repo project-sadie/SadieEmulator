ce6fe6e9a7d648c684123e93da82db9767ee5dd5
sam-habbo <habtard@gmail.com>
2022-02-15 17:42:59 +0000

    Improvements to rooms

--- Sadie.Game.Rooms/Users/RoomUserRepository.cs
+++ Sadie.Game.Rooms/Users/RoomUserRepository.cs
@@ -15,4 +15,12 @@ public class RoomUserRepository
     public bool TryAdd(RoomUser user) => _users.TryAdd(user.Id, user);
     public bool TryGet(long id, out RoomUser? user) => _users.TryGetValue(id, out user);
     public int Count => _users.Count;
+    
+    public async Task BroadcastDataToUsersAsync(byte[] data)
+    {
+        foreach (var roomUser in _users.Values)
+        {
+            await roomUser.NetworkObject.WriteToStreamAsync(data);
+        }
+    }
 }
--- Sadie.Networking.Packets/Client/Rooms/Users/Chat/RoomUserChatEvent.cs
+++ Sadie.Networking.Packets/Client/Rooms/Users/Chat/RoomUserChatEvent.cs
@@ -13,13 +13,13 @@ public class RoomUserChatEvent : INetworkPacketEvent
         _roomRepository = roomRepository;
     }
     
-    public Task HandleAsync(INetworkClient client, INetworkPacketReader reader)
+    public async Task HandleAsync(INetworkClient client, INetworkPacketReader reader)
     {
         var text = reader.ReadString();
 
-        if (text.Length > 100)
+        if (string.IsNullOrEmpty(text) || text.Length > 100)
         {
-            return Task.CompletedTask;
+            return;
         }
 
         var roomId = client.Player.LastRoomLoaded;
@@ -27,17 +27,12 @@ public class RoomUserChatEvent : INetworkPacketEvent
 
         if (!result || room == null || !room.UserRepository.TryGet(client.Player.Id, out var user) || user == null)
         {
-            return Task.CompletedTask;
+            return;
         }
 
         var bubbleColor = reader.ReadInt();
         var message = new RoomChatMessage(user, text, room, bubbleColor, 1);
         
-        foreach (var roomUser in room.UserRepository.GetAll())
-        {
-            roomUser.NetworkObject.WriteToStreamAsync(new RoomUserChatWriter(message).GetAllBytes());
-        }
-        
-        return Task.CompletedTask;
+        await room.UserRepository.BroadcastDataToUsersAsync(new RoomUserChatWriter(message).GetAllBytes());
     }
 }
