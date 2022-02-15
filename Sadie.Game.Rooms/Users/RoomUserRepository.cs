9fac868f50fc127b166e9752cb22ab8c01ef4555
sam-habbo <habtard@gmail.com>
2022-02-15 17:33:38 +0000

    Cleanup

--- Sadie.Game.Rooms/Users/RoomUserRepository.cs
+++ Sadie.Game.Rooms/Users/RoomUserRepository.cs
@@ -11,7 +11,7 @@ public class RoomUserRepository
         _users = users;
     }
 
-    public ICollection<RoomUser?> GetAll() => _users.Values;
+    public ICollection<RoomUser> GetAll() => _users.Values;
     public bool TryAdd(RoomUser user) => _users.TryAdd(user.Id, user);
     public bool TryGet(long id, out RoomUser? user) => _users.TryGetValue(id, out user);
     public int Count => _users.Count;
