40e81f31c5e8c07418f3e9d9caf38ad1b8c5d325
sam-habbo <habtard@gmail.com>
2022-02-25 00:36:13 +0000

    Create PacketEventHelpers.cs

---
+++ Sadie.Networking.Events/PacketEventHelpers.cs
@@ -0,0 +1,35 @@
+﻿using Sadie.Game.Rooms;
+using Sadie.Game.Rooms.Users;
+using Sadie.Networking.Client;
+
+namespace Sadie.Networking.Events;
+
+internal static class PacketEventHelpers
+{
+    internal static bool TryResolveRoomObjectsForClient(IRoomRepository roomRepository, INetworkClient client, out Room? room, out RoomUser? user)
+    {
+        if (client.Player == null)
+        {
+            room = null;
+            user = null;
+            
+            return false;
+        }
+        
+        var roomId = client.Player.LastRoomLoaded;
+        var (result, roomObject) = roomRepository.TryGetRoomById(roomId);
+
+        if (!result || roomObject == null || client.RoomUser == null)
+        {
+            room = null;
+            user = null;
+            
+            return false;
+        }
+
+        room = roomObject;
+        user = client.RoomUser;
+        
+        return true;
+    }
+}
