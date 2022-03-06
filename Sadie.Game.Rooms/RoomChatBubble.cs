0393aa6496f8ebdd697aa9800651735b54ec7234
sam-habbo <habtard@gmail.com>
2022-03-06 19:19:51 +0000

    Create RoomChatBubble.cs

---
+++ Sadie.Game.Rooms/RoomChatBubble.cs
@@ -0,0 +1,44 @@
+﻿namespace Sadie.Game.Rooms;
+
+public enum RoomChatBubble
+{
+    Default = 0,
+    Respect = 1,
+    Bot = 2,
+    Red = 3,
+    Blue = 4,
+    Yellow = 5,
+    Green = 6,
+    Gray = 7,
+    Unknown1 = 8, // TODO: name
+    Unknown2 = 9, // TODO: name
+    Unknown3 = 10, // TODO: name
+    SkyBlue = 11,
+    Pink = 12,
+    Purple = 13,
+    Orange = 14,
+    AlternativeBlue = 15,
+    Valentines = 16,
+    RoseBorder = 17,
+    Unknown4 = 18, // TODO: name
+    Pig = 19,
+    Unknown5 = 20, // TODO: name
+    Unknown6 = 21, // TODO: name
+    DragonFire = 22,
+    Unknown7 = 23, // TODO: name
+    Unknown8 = 24, // TODO: name
+    Unknown9 = 25, // TODO: name
+    Unknown10 = 26, // TODO: name
+    Unknown11 = 27, // TODO: name
+    Parrot = 28,
+    Unknown12 = 29, // TODO: name
+    Bot2 = 30,
+    Bot3 = 31,
+    Unknown13 = 32, // TODO: name
+    FrankHat = 33,
+    Alert = 34,
+    Unknown14 = 35, // TODO: name
+    Unknown15 = 36, // TODO: name
+    Unknown16 = 37, // TODO: name
+    Unknown17 = 38, // TODO: name
+}
