885c1752137e3e5fd9f517d90008d61909ae28e2
sam-habbo <habtard@gmail.com>
2022-03-06 19:17:44 +0000

    Create RoomUserLeftWriter.cs

---
+++ Sadie.Game.Rooms/Packets/RoomUserLeftWriter.cs
@@ -0,0 +1,13 @@
+﻿using Sadie.Shared.Networking;
+using Sadie.Shared.Networking.Packets;
+
+namespace Sadie.Game.Rooms.Packets;
+
+public class RoomUserLeftWriter : NetworkPacketWriter
+{
+    public RoomUserLeftWriter(long userId)
+    {
+        WriteShort(ServerPacketId.RoomUserLeft);
+        WriteString(userId.ToString());
+    }
+}
