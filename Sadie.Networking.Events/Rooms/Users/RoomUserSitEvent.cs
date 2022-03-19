0cf379c8afc212848444a0ddd34b4d97d8422775
sam-habbo <habtard@gmail.com>
2022-03-19 13:04:17 +0000

    RoomUserSitEvent

---
+++ Sadie.Networking.Events/Rooms/Users/RoomUserSitEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Drawing;
+using Sadie.Game.Rooms;
+using Sadie.Game.Rooms.Users;
+using Sadie.Networking.Client;
+using Sadie.Networking.Packets;
+
+namespace Sadie.Networking.Events.Rooms.Users;
+
+public class RoomUserSitEvent : INetworkPacketEvent
+{
+    private readonly IRoomRepository _roomRepository;
+
+    public RoomUserSitEvent(IRoomRepository roomRepository)
+    {
+        _roomRepository = roomRepository;
+    }
+
+    public async Task HandleAsync(INetworkClient client, INetworkPacketReader reader)
+    {
+        if (!PacketEventHelpers.TryResolveRoomObjectsForClient(_roomRepository, client, out var room, out var roomUser))
+        {
+            return;
+        }
+        
+        roomUser!.StatusMap[RoomUserStatus.Sit] = 0.5 + "";
+        await room!.UserRepository.UpdateStatusForUserAsync(roomUser);
+    }
+}
