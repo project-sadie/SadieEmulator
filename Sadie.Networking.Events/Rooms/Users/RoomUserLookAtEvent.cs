56d5f2c01a91a910172247890dffa4cc231fcbc0
sam-habbo <habtard@gmail.com>
2022-03-19 13:04:03 +0000

    RoomUserLookAtEvent

---
+++ Sadie.Networking.Events/Rooms/Users/RoomUserLookAtEvent.cs
@@ -0,0 +1,41 @@
+﻿using System.Drawing;
+using Sadie.Game.Rooms;
+using Sadie.Game.Rooms.Users;
+using Sadie.Networking.Client;
+using Sadie.Networking.Packets;
+
+namespace Sadie.Networking.Events.Rooms.Users;
+
+public class RoomUserLookAtEvent : INetworkPacketEvent
+{
+    private readonly IRoomRepository _roomRepository;
+
+    public RoomUserLookAtEvent(IRoomRepository roomRepository)
+    {
+        _roomRepository = roomRepository;
+    }
+
+    public async Task HandleAsync(INetworkClient client, INetworkPacketReader reader)
+    {
+        if (!PacketEventHelpers.TryResolveRoomObjectsForClient(_roomRepository, client, out _, out var roomUser))
+        {
+            return;
+        }
+
+        if (roomUser!.StatusMap.ContainsKey(RoomUserStatus.Move))
+        {
+            return;
+        }
+
+        var currentPoint = roomUser.Point;
+        var x = reader.ReadInt();
+        var y = reader.ReadInt();
+
+        if (currentPoint.X == x && currentPoint.Y == y)
+        {
+            return;
+        }
+
+        roomUser.LookAtPoint(new Point(x, y));
+    }
+}
