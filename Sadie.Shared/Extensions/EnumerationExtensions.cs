a4fe273a944dc87b42ead496f4adfedbc0652262
sam-habbo <habtard@gmail.com>
2022-04-07 13:34:33 +0100

    DistinctBy<T> enumeration extension method

---
+++ Sadie.Shared/Extensions/EnumerationExtensions.cs
@@ -0,0 +1,9 @@
+namespace Sadie.Shared.Extensions;
+
+public static class EnumerationExtensions
+{
+    public static IEnumerable<T> DistinctBy<T, TKey>(this IEnumerable<T> items, Func<T, TKey> property)
+    {
+        return items.GroupBy(property).Select(x => x.First());
+    }
+}
