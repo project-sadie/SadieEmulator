5a7137531d0248de6e0efd5e17168037c2774de6
sam-habbo <habtard@gmail.com>
2022-04-07 13:38:38 +0100

    Using enum for user actions

--- Sadie.Networking.Writers/Rooms/Users/RoomUserActionWriter.cs
+++ Sadie.Networking.Writers/Rooms/Users/RoomUserActionWriter.cs
@@ -1,14 +1,15 @@
-﻿using Sadie.Shared.Networking;
+﻿using Sadie.Game.Rooms.Users;
+using Sadie.Shared.Networking;
 using Sadie.Shared.Networking.Packets;
 
 namespace Sadie.Networking.Writers.Rooms.Users;
 
 public class RoomUserActionWriter : NetworkPacketWriter
 {
-    public RoomUserActionWriter(long userId, int actionId)
+    public RoomUserActionWriter(long userId, RoomUserAction action)
     {
         WriteShort(ServerPacketId.RoomUserAction);
         WriteLong(userId);
-        WriteLong(actionId);
+        WriteLong((int) action);
     }
 }
