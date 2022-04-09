fa368ea14bac5f2a6ab9f2790acaad2c3b9d2ed4
sam-habbo <habtard@gmail.com>
2022-04-09 12:11:48 +0100

    PlayerFriendsEvent

---
+++ Sadie.Networking.Events/Players/Friendships/PlayerFriendsEvent.cs
@@ -0,0 +1,13 @@
+﻿using Sadie.Networking.Client;
+using Sadie.Networking.Packets;
+using Sadie.Networking.Writers.Players.Friends;
+
+namespace Sadie.Networking.Events.Players.Friendships;
+
+public class PlayerFriendsEvent : INetworkPacketEvent
+{
+    public async Task HandleAsync(INetworkClient client, INetworkPacketReader reader)
+    {
+        await client.WriteToStreamAsync(new PlayerFriendsListWriter(0, 0, 0).GetAllBytes());
+    }
+}
