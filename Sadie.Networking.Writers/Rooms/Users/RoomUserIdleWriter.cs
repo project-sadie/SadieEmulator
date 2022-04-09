830a54ef5f6cc1f107c704f9c2fafbb98100a719
sam-habbo <habtard@gmail.com>
2022-04-09 12:08:29 +0100

    RoomUserIdleWriter

---
+++ Sadie.Networking.Writers/Rooms/Users/RoomUserIdleWriter.cs
@@ -0,0 +1,15 @@
+﻿using Sadie.Game.Rooms.Users;
+using Sadie.Shared.Networking;
+using Sadie.Shared.Networking.Packets;
+
+namespace Sadie.Networking.Writers.Rooms.Users;
+
+public class RoomUserIdleWriter : NetworkPacketWriter
+{
+    public RoomUserIdleWriter(IRoomUser user)
+    {
+        WriteShort(ServerPacketId.RoomUserIdle);
+        WriteInt(user.Id);
+        WriteBoolean(user.IsIdle);
+    }
+}
