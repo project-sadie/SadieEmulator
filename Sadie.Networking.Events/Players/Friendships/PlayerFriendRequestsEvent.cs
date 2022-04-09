71973c24b146740725b7714ed44ad8f8ea051266
sam-habbo <habtard@gmail.com>
2022-04-09 12:11:37 +0100

    PlayerFriendRequestsEvent

---
+++ Sadie.Networking.Events/Players/Friendships/PlayerFriendRequestsEvent.cs
@@ -0,0 +1,24 @@
+﻿using Sadie.Game.Players.Friendships;
+using Sadie.Networking.Client;
+using Sadie.Networking.Packets;
+using Sadie.Networking.Writers.Players.Messenger;
+
+namespace Sadie.Networking.Events.Players.Friendships;
+
+public class PlayerFriendRequestsEvent : INetworkPacketEvent
+{
+    private readonly IPlayerFriendshipRepository _friendshipRepository;
+
+    public PlayerFriendRequestsEvent(IPlayerFriendshipRepository friendshipRepository)
+    {
+        _friendshipRepository = friendshipRepository;
+    }
+    
+    public async Task HandleAsync(INetworkClient client, INetworkPacketReader reader)
+    {
+        var pendingFriends = await _friendshipRepository.
+            GetFriendshipRecords(client.Player.Id, PlayerFriendshipStatus.Pending);
+        
+        await client.WriteToStreamAsync(new PlayerFriendRequestsWriter(pendingFriends).GetAllBytes());
+    }
+}
