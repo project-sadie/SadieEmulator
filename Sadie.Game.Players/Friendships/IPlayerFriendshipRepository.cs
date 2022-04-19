b7135822202741a992d1eba10e4dea79fa14fac6
sam-habbo <habtard@gmail.com>
2022-04-20 00:24:40 +0100

    Cleanup

--- Sadie.Game.Players/Friendships/IPlayerFriendshipRepository.cs
+++ Sadie.Game.Players/Friendships/IPlayerFriendshipRepository.cs
@@ -1,5 +1,3 @@
-using System.Runtime.CompilerServices;
-
 namespace Sadie.Game.Players.Friendships;
 
 public interface IPlayerFriendshipRepository
--- Sadie.Game.Players/PlayerFactory.cs
+++ Sadie.Game.Players/PlayerFactory.cs
@@ -1,12 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Sadie.Database;
-using Sadie.Game.Players.Badges;
 using Sadie.Game.Players.Balance;
-using Sadie.Game.Players.Components;
-using Sadie.Game.Players.Friendships;
 using Sadie.Game.Players.Navigator;
-using Sadie.Shared.Game.Avatar;
 using Sadie.Shared.Networking;
 
 namespace Sadie.Game.Players;
