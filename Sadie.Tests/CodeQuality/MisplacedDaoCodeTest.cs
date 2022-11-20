e6fe6a49b62a6e01c834710e3522b19df987dfc7
sam-habbo <habtard@gmail.com>
2022-11-20 20:51:15 +0000

    Unit test refactoring

--- Sadie.Tests/CodeQuality/MisplacedDaoCodeTest.cs
+++ Sadie.Tests/CodeQuality/MisplacedDaoCodeTest.cs
@@ -2,35 +2,27 @@ namespace Sadie.Tests.Other;
 
 public class MisplacedDaoCodeTest
 {
+    private readonly List<string> ignoredFiles = new()
+    {
+        "IDatabaseProvider.cs",
+        "DatabaseProvider.cs",
+        "IDatabaseConnection.cs",
+        "DatabaseConnection.cs",
+        "DatabaseServiceCollection.cs",
+        "Server.cs",
+        "MisplacedDaoCodeTest.cs",
+    };
+    
     /// <summary>
     /// Ensures no database queries are ran outside of dao classes
     /// </summary>
     [Test]
     public Task No_Non_Dao_Class_Should_Have_Database_Access_Code()
     {
-        var ignoredFiles = new List<string>
-        {
-            "IDatabaseProvider.cs",
-            "DatabaseProvider.cs",
-            "IDatabaseConnection.cs",
-            "DatabaseConnection.cs",
-            "DatabaseServiceCollection.cs",
-            "Server.cs",
-        };
-        
         var path = Directory.GetParent(Directory.GetCurrentDirectory())?.Parent?.Parent.Parent.FullName;
         
-        foreach (var file in Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories))
+        foreach (var file in Directory.GetFiles(path, "*.cs", SearchOption.AllDirectories).Where(x => !x.EndsWith("Dao.cs") && !ignoredFiles.Contains(Path.GetFileName(x))))
         {
-            var fileName = Path.GetFileName(file);
-            
-            if (file.Contains("Sadie.Tests") || file.EndsWith("Dao.cs") || ignoredFiles.Contains(fileName))
-            {
-                continue;
-            }
-            
-            Console.WriteLine(file);
-            
             foreach (var line in File.ReadAllLines(file))
             {
                 Assert.That(line, Does.Not.Contain("IDatabaseProvider"));
