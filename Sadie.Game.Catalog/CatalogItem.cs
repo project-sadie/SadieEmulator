b24337a06ed04b2df441d1381f9cf17015c67a93
sam-habbo <habtard@gmail.com>
2022-12-08 14:50:21 +0000

    Catalog updates

--- Sadie.Game.Catalog/CatalogItem.cs
+++ Sadie.Game.Catalog/CatalogItem.cs
@@ -5,11 +5,25 @@ public class CatalogItem
     public int Id { get; }
     public string Name { get; }
     public int CostCredits { get; }
-    
-    public CatalogItem(int id, string name, int costCredits)
+    public int CostPoints { get; }
+    public int CostPointsType { get; }
+    public bool CanGift { get; }
+    public bool RequiresClubMembership { get; }
+    public int Amount { get; }
+    public int StackLimit { get; }
+    public int SellLimit { get; }
+
+    public CatalogItem(int id, string name, int costCredits, int costPoints, int costPointsType, bool canGift, bool requiresClubMembership, int amount, int stackLimit, int sellLimit)
     {
         Id = id;
         Name = name;
         CostCredits = costCredits;
+        CostPoints = costPoints;
+        CostPointsType = costPointsType;
+        CanGift = canGift;
+        RequiresClubMembership = requiresClubMembership;
+        Amount = amount;
+        StackLimit = stackLimit;
+        SellLimit = sellLimit;
     }
 }
