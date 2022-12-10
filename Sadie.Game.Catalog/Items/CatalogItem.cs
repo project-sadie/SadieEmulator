68ace24b2b5320138db0a771348d0d5c2a6e79de
sam-habbo <habtard@gmail.com>
2022-12-10 17:51:51 +0000

    Cleaning up folder structure

--- Sadie.Game.Catalog/Items/CatalogItem.cs
+++ Sadie.Game.Catalog/Items/CatalogItem.cs
@@ -1,3 +1,5 @@
+using Sadie.Game.Furniture;
+
 namespace Sadie.Game.Catalog;
 
 public class CatalogItem
@@ -7,20 +9,22 @@ public class CatalogItem
     public int CostCredits { get; }
     public int CostPoints { get; }
     public int CostPointsType { get; }
-    public bool CanGift { get; }
+    public int FurnitureItemId { get; }
     public bool RequiresClubMembership { get; }
+    public string Metadata { get; }
     public int Amount { get; }
     public int SellLimit { get; }
 
-    public CatalogItem(int id, string name, int costCredits, int costPoints, int costPointsType, bool canGift, bool requiresClubMembership, int amount, int sellLimit)
+    public CatalogItem(int id, string name, int costCredits, int costPoints, int costPointsType, int furnitureItemId, bool requiresClubMembership, string metadata, int amount, int sellLimit)
     {
         Id = id;
         Name = name;
         CostCredits = costCredits;
         CostPoints = costPoints;
         CostPointsType = costPointsType;
-        CanGift = canGift;
+        FurnitureItemId = furnitureItemId;
         RequiresClubMembership = requiresClubMembership;
+        Metadata = metadata;
         Amount = amount;
         SellLimit = sellLimit;
     }
