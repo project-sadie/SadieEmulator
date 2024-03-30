dd1a5da0febee24cd2c11cc5361e9fcf643c92e9
habtard <habtard@gmail.com>
2024-03-30 11:52:58 +0000

    380 -> 340

--- Sadie.Tests/CodeQuality/ClassLineCountTest.cs
+++ Sadie.Tests/CodeQuality/ClassLineCountTest.cs
@@ -2,7 +2,7 @@ namespace Sadie.Tests.CodeQuality;
 
 public class ClassLineCountTest
 {
-    private const int MaxLinesForClass = 380;
+    private const int MaxLinesForClass = 340;
  
     private readonly List<string> _excludedFiles = new();
     
