9275c7c1696b9d14bb57b4839c56d6fcabb01752
habtard <habtard@gmail.com>
2024-04-03 10:40:35 +0100

    Added serialization for layouts; pets, guilds, trax, club gifts etc

--- Sadie.Networking.Writers/Catalog/CatalogPageWriter.cs
+++ Sadie.Networking.Writers/Catalog/CatalogPageWriter.cs
@@ -18,6 +18,7 @@ public class CatalogPageWriter : NetworkPacketWriter
         string primaryText,
         string secondaryText,
         string teaserText,
+        string detailsText,
         List<CatalogItem> items, 
         string catalogMode,
         bool acceptSeasonCurrencyAsCredits,
@@ -29,8 +30,113 @@ public class CatalogPageWriter : NetworkPacketWriter
 
         switch (pageLayout)
         {
+            case "guilds":
+                WriteString("guild_frontpage");
+                WriteInteger(2);
+                WriteString(headerImage);
+                WriteString(teaserImage);
+                WriteInteger(3);
+                WriteString(primaryText);
+                WriteString(detailsText);
+                WriteString(teaserText);
+                break;
+            case "guild_custom_furni":
+                WriteString(pageLayout);
+                WriteInteger(2);
+                WriteString(headerImage);
+                WriteString(teaserImage);
+                WriteInteger(3);
+                WriteString(primaryText);
+                WriteString(detailsText);
+                WriteString(teaserText);
+                break;
+            case "guild_forum":
+                WriteString(pageLayout);
+                WriteInteger(2);
+                WriteString(headerImage);
+                WriteString(teaserImage);
+                WriteInteger(3);
+                WriteString(primaryText);
+                WriteString(detailsText);
+                WriteString(teaserText);
+                break;
+            case "soundmachine":
+                WriteString(pageLayout);
+                WriteInteger(2);
+                WriteString(headerImage);
+                WriteString(teaserImage);
+                WriteInteger(2);
+                WriteString(primaryText);
+                WriteString(detailsText);
+                break;
+            case "marketplace_own_items":
+            case "marketplace":
+                WriteString(pageLayout);
+                WriteInteger(0);
+                WriteInteger(0);
+                break;
+            case "club_gifts":
+                WriteString(pageLayout);
+                WriteInteger(1);
+                WriteString(headerImage);
+                WriteInteger(1);
+                WriteString(primaryText);
+                break;
+            case "info_loyalty":
+                WriteString(pageLayout);
+                WriteInteger(1);
+                WriteString(headerImage);
+                WriteInteger(1);
+                WriteString(primaryText);
+                WriteInteger(0);
+                break;
+            case "room_bundle":
+            case "single_bundle":
+                WriteString("single_bundle");
+                WriteInteger(3);
+                WriteString(headerImage);
+                WriteString(teaserImage);
+                WriteString("");
+                WriteInteger(4);
+                WriteString(primaryText);
+                WriteString(detailsText);
+                WriteString(teaserText);
+                WriteString(secondaryText);
+                break;
+            case "badge_display":
+            case "petcustomization":
+                WriteString(pageLayout);
+                WriteInteger(3);
+                WriteString(headerImage);
+                WriteString(teaserImage);
+                WriteString(specialImage);
+                WriteInteger(3);
+                WriteString(primaryText);
+                WriteString(detailsText);
+                WriteString(teaserText);
+                break;
+            case "pets":
+                WriteString(pageLayout);
+                WriteInteger(2);
+                WriteString(headerImage);
+                WriteString(teaserImage);
+                WriteInteger(4);
+                WriteString(primaryText);
+                WriteString(secondaryText);
+                WriteString(detailsText);
+                WriteString(teaserText);
+                break;
+            case "bots":
+                WriteString(pageLayout);
+                WriteInteger(2);
+                WriteString(headerImage);
+                WriteString(teaserImage);
+                WriteInteger(3);
+                WriteString(primaryText);
+                WriteString(detailsText);
+                WriteString(secondaryText);
+                break;
             case "vip_buy":
-            case "club_buy":
                 WriteString(pageLayout);
                 WriteInteger(2);
                 WriteString(headerImage);
@@ -47,8 +153,21 @@ public class CatalogPageWriter : NetworkPacketWriter
                 WriteString(secondaryText);
                 WriteString(teaserText);
                 break;
+            case "default_3x3_color_grouping":
+            case "spaces_new":
+            case "trophies":
+                WriteString(pageLayout);
+                WriteInteger(3);
+                WriteString(headerImage);
+                WriteString(teaserImage);
+                WriteString(specialImage);
+                WriteInteger(3);
+                WriteString(primaryText);
+                WriteString(detailsText);
+                WriteString(teaserText);
+                break;
             case "default_3x3":
-            Ccase "spaces_new":
+            case "recent_purchases":
                 WriteString(pageLayout);
                 WriteInteger(3);
                 WriteString(headerImage);
@@ -58,6 +177,15 @@ public class CatalogPageWriter : NetworkPacketWriter
                 WriteString(primaryText);
                 WriteString(secondaryText);
                 WriteString(teaserText);
+                break;
+            case "roomads":
+                WriteString(pageLayout);
+                WriteInteger(2);
+                WriteString(headerImage);
+                WriteString(teaserImage);
+                WriteInteger(2);
+                WriteString(primaryText);
+                WriteString(secondaryText);
                 break;
         }
         
