f78b525fa79fb0d708e1c5a677e17f022f28e93a
habtard <habtard@gmail.com>
2024-04-08 12:06:35 +0100

    Forgot setters

--- Sadie.Database/Models/Players/PlayerRole.cs
+++ Sadie.Database/Models/Players/PlayerRole.cs
@@ -2,7 +2,7 @@ namespace Sadie.Database.Models.Players;
 
 public class PlayerRole
 {
-    public int Id { get; }
-    public string Name { get; }
+    public int Id { get; set; }
+    public string Name { get; set; }
     public List<PlayerPermission> Permissions { get; set; }
 }
