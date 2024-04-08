0860ab20d630be6cfa17430fa244bd3319b7c931
habtard <habtard@gmail.com>
2024-04-08 12:06:00 +0100

    AvatarGender enum attributes

--- Sadie.Shared/Unsorted/Game/Avatar/AvatarGender.cs
+++ Sadie.Shared/Unsorted/Game/Avatar/AvatarGender.cs
@@ -1,7 +1,12 @@
-﻿namespace Sadie.Shared.Unsorted.Game.Avatar;
+﻿using System.ComponentModel;
+
+namespace Sadie.Shared.Unsorted.Game.Avatar;
 
 public enum AvatarGender
 {
+    [Description("M")]
     Male = 0,
+    
+    [Description("F")]
     Female = 1
 }
