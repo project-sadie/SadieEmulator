8307dfdd79d80be63c791f612eb88cd1f6d24bf4
habtard <habtard@gmail.com>
2024-04-08 12:06:15 +0100

    Added player relationships to PlayerRespect

--- Sadie.Database/Models/Players/PlayerRespect.cs
+++ Sadie.Database/Models/Players/PlayerRespect.cs
@@ -4,5 +4,7 @@ public class PlayerRespect
 {
     public int Id { get; set; }
     public int OriginPlayerId { get; set; }
+    public Player OriginPlayer { get; set; }
     public int TargetPlayerId { get; set; }
+    public Player TargetPlayer { get; set; }
 }
