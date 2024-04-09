49d3d459f52606060fd372d119bf307b1095bd13
habtard <habtard@gmail.com>
2024-04-09 07:10:03 +0100

    Added playerdata to context

--- Sadie.Database/SadieContext.cs
+++ Sadie.Database/SadieContext.cs
@@ -33,6 +33,7 @@ public class SadieContext(DbContextOptions options) : DbContext(options)
     public DbSet<RoomLayout> RoomLayouts { get; set; }
     public DbSet<Room> Rooms { get; set; }
     public DbSet<Player> Players { get; set; }
+    public DbSet<PlayerData> PlayerData { get; set; }
     public DbSet<PlayerBadge> PlayerBadges { get; set; }
     public DbSet<Badge> Badges { get; set; }
     public DbSet<CatalogClubOffer> CatalogClubOffers { get; set; }
