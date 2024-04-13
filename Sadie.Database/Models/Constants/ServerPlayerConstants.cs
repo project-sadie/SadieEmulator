6be02b5ecefa769be6fe891104f3b2b6e44e55ee
habtard <habtard@gmail.com>
2024-04-13 12:03:41 +0100

    Missing files

---
+++ Sadie.Database/Models/Constants/ServerPlayerConstants.cs
@@ -0,0 +1,8 @@
+namespace Sadie.Database.Models.Constants;
+
+public class ServerPlayerConstants
+{
+    public int MaxMottoLength { get; set; }
+    public int MinSsoLength { get; set; }
+    public int MaxFriendships { get; set; }
+}
---
+++ Sadie.Database/Models/Constants/ServerRoomConstants.cs
@@ -0,0 +1,10 @@
+namespace Sadie.Database.Models.Constants;
+
+public class ServerRoomConstants
+{
+    public int MaxChatMessageLength { get; set; }
+    public int SecondsTillUserIdle { get; set; }
+    public int MaxNameLength { get; set; }
+    public int MaxDescriptionLength { get; set; }
+    public int MaxTagLength { get; set; }
+}
