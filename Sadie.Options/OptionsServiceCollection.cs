9d79c16789d9cfe946000a7382f184bb0a496582
Damien Jolly <DamienJolly@hotmail.com>
2024-04-15 02:17:17 +0100

    Added database validation.

--- Sadie.Options/OptionsServiceCollection.cs
+++ Sadie.Options/OptionsServiceCollection.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Sadie.Options.Models;
+using Sadie.Options.Validation;
 
 namespace Sadie.Options;
 
@@ -13,5 +15,7 @@ public static class OptionsServiceCollection
         serviceCollection.Configure<DatabaseOptions>(options => config.GetSection("DatabaseOptions").Bind(options));
         serviceCollection.Configure<NetworkOptions>(options => config.GetSection("NetworkOptions").Bind(options));
         serviceCollection.Configure<NetworkPacketOptions>(options => config.GetSection("NetworkOptions:PacketOptions").Bind(options));
+
+        serviceCollection.AddSingleton<IValidateOptions<DatabaseOptions>, DatabaseOptionsValidator>();
     }
 }
---
+++ Sadie.Options/Validation/DatabaseOptionsValidator.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.Options;
+using Sadie.Options.Models;
+
+namespace Sadie.Options.Validation;
+
+public class DatabaseOptionsValidator : IValidateOptions<DatabaseOptions>
+{
+    public ValidateOptionsResult Validate(string? name, DatabaseOptions options)
+    {
+        if (options == null)
+        {
+            return ValidateOptionsResult.Fail("Configuration object is null.");
+        }
+
+        if (string.IsNullOrWhiteSpace(options.Host))
+        {
+            return ValidateOptionsResult.Fail("Host cannot be null or empty.");
+        }
+
+        if (string.IsNullOrWhiteSpace(options.Username))
+        {
+            return ValidateOptionsResult.Fail("Username cannot be null or empty.");
+        }
+
+        if (string.IsNullOrWhiteSpace(options.Password))
+        {
+            return ValidateOptionsResult.Fail("Password cannot be null or empty.");
+        }
+
+        if (string.IsNullOrWhiteSpace(options.Database))
+        {
+            return ValidateOptionsResult.Fail("Database cannot be null or empty.");
+        }
+
+        return ValidateOptionsResult.Success;
+    }
+}
