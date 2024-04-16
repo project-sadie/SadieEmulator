f8324b0734a071e9d97301a08645ae221065cdca
habtard <habtard@gmail.com>
2024-04-16 13:46:34 +0100

    Using packet options

--- Sadie.Networking/NetworkListener.cs
+++ Sadie.Networking/NetworkListener.cs
@@ -17,10 +17,13 @@ namespace Sadie.Networking
     public class NetworkListener(
         ILogger<NetworkListener> logger,
         IOptions<NetworkOptions> options,
+        IOptions<NetworkPacketOptions> packetOptions,
         DefaultChannelHandler defaultChannelHandler)
         : INetworkListener
     {
         private readonly NetworkOptions _networkOptions = options.Value;
+        private readonly NetworkPacketOptions _packetOptions = packetOptions.Value;
+        
         private ServerBootstrap _bootstrap;
         private IChannel? _channel;
 
@@ -32,9 +35,9 @@ namespace Sadie.Networking
                 .ChildOption(ChannelOption.TcpNodelay, true)
                 .ChildOption(ChannelOption.SoKeepalive, true)
                 .ChildOption(ChannelOption.SoReuseaddr, true)
-                .ChildOption(ChannelOption.SoRcvbuf, 4096)
+                .ChildOption(ChannelOption.SoRcvbuf, _packetOptions.BufferByteSize)
                 .Option(ChannelOption.SoBacklog, 8192)
-                .ChildOption(ChannelOption.RcvbufAllocator, new FixedRecvByteBufAllocator(4096))
+                .ChildOption(ChannelOption.RcvbufAllocator, new FixedRecvByteBufAllocator(_packetOptions.BufferByteSize))
                 .ChildOption(ChannelOption.Allocator, UnpooledByteBufferAllocator.Default)
                 .ChildHandler(new ActionChannelInitializer<IChannel>(channel =>
                 {
@@ -50,7 +53,12 @@ namespace Sadie.Networking
                     pipeline.AddLast(new HttpObjectAggregator(65536));
                     pipeline.AddLast(new WebSocketServerProtocolHandler("/", null, true, 65536, false, true));
                     pipeline.AddLast(new WebSocketCodec());
-                    pipeline.AddLast(new LengthFieldBasedFrameDecoder(65536, 0, 4, 0, 4));
+                    pipeline.AddLast(new LengthFieldBasedFrameDecoder(
+                        65536, 
+                        0, 
+                        _packetOptions.FrameLengthByteCount, 
+                        0, 
+                        _packetOptions.FrameLengthByteCount));
                     pipeline.AddLast(new PacketDecoder());
                     pipeline.AddLast(new PacketEncoder());
                     pipeline.AddLast(defaultChannelHandler);
