5b9958a821da333c38a7edda988e4cfca17a59e4
Damien Jolly <DamienJolly@hotmail.com>
2024-04-17 21:35:49 +0100

    Refactored MOTD to use the appropriate event.

---
+++ Sadie.Networking.Events/Handlers/Notifications/MOTDEventHandler.cs
@@ -0,0 +1,26 @@
+﻿using Sadie.Database.Models;
+using Sadie.Networking.Client;
+using Sadie.Networking.Packets;
+using Sadie.Shared;
+using Sadie.Shared.Unsorted;
+
+namespace Sadie.Networking.Events.Handlers.Notifications;
+
+public class MOTDEventHandler(ServerSettings serverSettings) : INetworkPacketEventHandler
+{
+    public int Id => EventHandlerIds.MOTD;
+
+    public async Task HandleAsync(INetworkClient client, INetworkPacketReader reader)
+    {
+        if (string.IsNullOrEmpty(serverSettings.PlayerWelcomeMessage))
+        {
+            return;
+        }
+
+        var formattedMessage = serverSettings.PlayerWelcomeMessage
+            .Replace("[username]", client.Player.Username)
+            .Replace("[version]", GlobalState.Version.ToString());
+
+        await client.Player.NetworkObject.WriteToStreamAsync(new PlayerAlertWriter(formattedMessage).GetAllBytes());
+    }
+}
