c168ce040f6f869b26cf50a6d8b0e3e8ed119527
habtard <habtard@gmail.com>
2024-04-21 13:48:45 +0100

    Sit command

---
+++ Sadie.Game.Rooms/Chat/Commands/General/SitCommand.cs
@@ -0,0 +1,21 @@
+using Sadie.Game.Rooms.Users;
+using Sadie.Shared.Unsorted;
+
+namespace Sadie.Game.Rooms.Chat.Commands.General;
+
+public class SitCommand : AbstractRoomChatCommand
+{
+    public override string Trigger => "sit";
+    public override string Description => "Makes your avatar sit down";
+    
+    public override Task ExecuteAsync(IRoomUser user)
+    {
+        if (user.StatusMap.ContainsKey(RoomUserStatus.Sit))
+        {
+            return Task.CompletedTask;
+        }
+        
+        user.AddStatus(RoomUserStatus.Sit, "0");
+        return Task.CompletedTask;
+    }
+}
