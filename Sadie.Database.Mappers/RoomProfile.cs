7ca58c66853b73b8dfb009c42c1e8c588f9a5f3d
habtard <habtard@gmail.com>
2024-04-21 06:22:49 +0100

    Fix for map generation

--- Sadie.Database.Mappers/RoomProfile.cs
+++ Sadie.Database.Mappers/RoomProfile.cs
@@ -16,7 +16,7 @@ public class RoomProfile : Profile
                 x.Id,
                 x.Name,
                 x.Layout,
-                new RoomTileMap(x.Layout.HeightMap, RoomHelpers.GenerateMapForRoom(x.Layout.MapSizeY, x.Layout.MapSizeX, x.Layout.HeightMap, x.FurnitureItems)),
+                new RoomTileMap(x.Layout.HeightMap, RoomHelpers.GenerateMapForRoom(x.Layout.MapSizeX, x.Layout.MapSizeY, x.Layout.HeightMap, x.FurnitureItems)),
                 x.Owner,
                 x.Description,
                 x.MaxUsersAllowed,
