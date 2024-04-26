7c29fb61b5399bd7ecb3bb0832ea0635b8c00a25
habtard <habtard@gmail.com>
2024-04-26 17:44:03 +0100

    More updatesssss

--- Sadie.Networking.Writers/Players/Clothing/PlayerClothingListWriter.cs
+++ Sadie.Networking.Writers/Players/Clothing/PlayerClothingListWriter.cs
@@ -1,16 +1,16 @@
 ﻿using Sadie.Shared.Unsorted.Networking;
 using Sadie.Shared.Unsorted.Networking.Packets;
+using Sadie.Shared.Unsorted.Networking.Packets.Attributes;
 
 namespace Sadie.Networking.Writers.Players.Clothing;
 
+[PacketId(ServerPacketId.PlayerClothingList)]
 public class PlayerClothingListWriter : AbstractPacketWriter
 {
-    // TODO ...
-    
-    public PlayerClothingListWriter()
+    public override void OnSerialize(NetworkPacketWriter writer)
     {
-        WriteShort(ServerPacketId.PlayerClothingList);
-        WriteInteger(0);
-        WriteInteger(0);
+        writer.WriteShort(ServerPacketId.PlayerClothingList);
+        writer.WriteInteger(0);
+        writer.WriteInteger(0);
     }
 }
