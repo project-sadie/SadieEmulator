f5307ebf5c1134fafa9e1c2ab4b4dde2c31b1fa7
habtard <habtard@gmail.com>
2024-04-27 15:05:47 +0100

    Updates

--- Sadie.Networking.Writers/Rooms/Users/RoomUserRespectWriter.cs
+++ Sadie.Networking.Writers/Rooms/Users/RoomUserRespectWriter.cs
@@ -1,14 +1,12 @@
 using Sadie.Networking.Serialization;
 using Sadie.Shared.Unsorted.Networking;
+using Sadie.Shared.Unsorted.Networking.Packets.Attributes;
 
 namespace Sadie.Networking.Writers.Rooms.Users;
 
+[PacketId(ServerPacketId.RoomUserRespect)]
 public class RoomUserRespectWriter : AbstractPacketWriter
 {
-    public RoomUserRespectWriter(int userId, int totalRespects)
-    {
-        WriteShort(ServerPacketId.RoomUserRespect);
-        WriteInteger(userId);
-        WriteInteger(totalRespects);
-    }
+    public required int UserId { get; init; }
+    public required int TotalRespects { get; init; }
 }
