0d1407be5cfaeefe8dc9f499c3eb07c4b1479a8c
Damien Jolly <DamienJolly@hotmail.com>
2024-05-12 16:50:47 +0100

    Add service collection.

---
+++ Sadie.Networking.Encryption/NetworkEncryptionServiceCollection.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace Sadie.Networking.Encryption;
+
+public static class NetworkEncryptionServiceCollection
+{
+    public static void AddServices(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<HabboEncryption>();
+    }
+}
