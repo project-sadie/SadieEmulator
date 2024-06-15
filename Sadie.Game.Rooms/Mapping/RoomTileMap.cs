3e81289d27124be1b3d52e83d00d331128841c69
habtard <habtard@gmail.com>
2024-06-15 18:33:43 +0100

    Redone tile mapping

--- Sadie.Game.Rooms/Mapping/RoomTileMap.cs
+++ Sadie.Game.Rooms/Mapping/RoomTileMap.cs
@@ -10,21 +10,52 @@ namespace Sadie.Game.Rooms.Mapping;
 
 public class RoomTileMap : IRoomTileMap
 {
-    private List<string> HeightmapRows { get; }
     public int SizeX { get; }
     public int SizeY { get; }
     public int Size { get; }
     public short[,] Map { get; }
     public ConcurrentDictionary<Point, List<IRoomUser>> UserMap { get; } = [];
     public ConcurrentDictionary<Point, List<IRoomBot>> BotMap { get; } = [];
+    public short[,] ZMap { get; set; }
+    public short[,] TileExistenceMap { get; set; }
 
     public RoomTileMap(string heightmap, ICollection<PlayerFurnitureItemPlacementData> furnitureItems)
     {
-        HeightmapRows = heightmap.Split("\r\n").ToList();
-        SizeX = HeightmapRows.MaxBy(x => x.Length)!.Length;
-        SizeY = HeightmapRows.Count;
+        var heightmapLines = heightmap.Replace("\n", "").Split("\r").ToList();
+        
+        SizeX = heightmapLines[0].Length;
+        SizeY = heightmapLines.Count;
         Size = SizeY * SizeX;
-        Map = RoomTileMapHelpers.BuildTileMapForRoom(SizeX, SizeY, heightmap, furnitureItems);
+        Map = new short[SizeY, SizeX];
+        ZMap = new short[SizeY, SizeX];
+        TileExistenceMap = new short[SizeY, SizeX];
+        
+        for (var y = 0; y < SizeY; y++)
+        {
+            for (var x = 0; x < SizeX; x++)
+            {
+                if (heightmapLines[y].Length != SizeX)
+                {
+                    break;
+                }
+
+                var square = heightmapLines[y][x].ToString().ToUpper();
+                
+                if (square == "X")
+                {
+                    continue;
+                }
+
+                if (!short.TryParse(square, out var height))
+                {
+                    height = (short) (10 + "ABCDEFGHIJKLMNOPQRSTUVWXYZ".IndexOf(square));
+                }
+
+                Map[y, x] = RoomTileMapHelpers.GetStateNumberForTile(x, y, furnitureItems);
+                ZMap[y, x] = height;
+                TileExistenceMap[y, x] = 1;
+            }
+        }
     }
 
     public void AddUserToMap(Point point, IRoomUser user) => 
