1bc2254869637bed5ebb7fd0bad2636ee9ecf963
habtard <habtard@gmail.com>
2024-06-18 16:52:45 +0100

    Updates

--- Sadie.Game.Rooms/Furniture/RoomFurnitureItemHelpers.cs
+++ Sadie.Game.Rooms/Furniture/RoomFurnitureItemHelpers.cs
@@ -1,6 +1,7 @@
 using Sadie.API.Game.Rooms;
 using Sadie.Database;
 using Sadie.Database.Models.Players.Furniture;
+using Sadie.Enums;
 using Sadie.Game.Rooms.Packets.Writers;
 using Sadie.Networking.Serialization;
 using Sadie.Shared.Unsorted;
@@ -49,25 +50,58 @@ public class RoomFurnitureItemHelpers
         PlayerFurnitureItemPlacementData roomFurnitureItem)
     {
         AbstractPacketWriter itemWriter = roomFurnitureItem.FurnitureItem.Type == FurnitureItemType.Floor ? new RoomFloorItemUpdatedWriter
-        {
-            Id = roomFurnitureItem.PlayerFurnitureItemId,
-            AssetId = roomFurnitureItem.FurnitureItem.AssetId,
-            PositionX = roomFurnitureItem.PositionX,
-            PositionY = roomFurnitureItem.PositionY,
-            Direction = (int)roomFurnitureItem.Direction,
-            PositionZ = roomFurnitureItem.PositionZ,
-            StackHeight = 0.ToString(),
-            Extra = 0,
-            ObjectDataKey = (int)ObjectDataKey.LegacyKey,
-            ExtraData = roomFurnitureItem.PlayerFurnitureItem.MetaData,
-            Expires = -1,
-            InteractionModes = 1,
-            OwnerId = roomFurnitureItem.PlayerFurnitureItem.PlayerId,
-        } : new RoomWallFurnitureItemUpdatedWriter
+            {
+                Id = roomFurnitureItem.PlayerFurnitureItemId,
+                AssetId = roomFurnitureItem.FurnitureItem.AssetId,
+                PositionX = roomFurnitureItem.PositionX,
+                PositionY = roomFurnitureItem.PositionY,
+                Direction = (int)roomFurnitureItem.Direction,
+                PositionZ = roomFurnitureItem.PositionZ,
+                StackHeight = 0.ToString(),
+                Extra = 0,
+                ObjectDataKey = (int) GetObjectDataKeyForItem(roomFurnitureItem),
+                ObjectData = GetObjectDataForItem(roomFurnitureItem),
+                MetaData = roomFurnitureItem.PlayerFurnitureItem.MetaData,
+                Expires = -1,
+                InteractionModes = 1,
+                OwnerId = roomFurnitureItem.PlayerFurnitureItem.PlayerId,
+            }
+            : new RoomWallFurnitureItemUpdatedWriter
         {
             Item = roomFurnitureItem
         };
         
         await room.UserRepository.BroadcastDataAsync(itemWriter);
+    }
+
+    public static ObjectDataKey GetObjectDataKeyForItem(PlayerFurnitureItemPlacementData furnitureItem)
+    {
+        if (furnitureItem.FurnitureItem.InteractionType == FurnitureItemInteractionType.RoomAdsBg)
+        {
+            return ObjectDataKey.MapKey;
+        }
+        
+        return ObjectDataKey.LegacyKey;
+    }
+
+    public static Dictionary<string, string> GetObjectDataForItem(PlayerFurnitureItemPlacementData furnitureItem)
+    {
+        if (furnitureItem.FurnitureItem!.InteractionType == FurnitureItemInteractionType.RoomAdsBg)
+        {
+            var data = new Dictionary<string, string>();
+            
+            foreach (var piece in furnitureItem.PlayerFurnitureItem.MetaData.Split(";"))
+            {
+                var parts = piece.Split("=");
+                var key = parts[0];
+                var value = parts.Length < 2 ? "" : parts[1];
+
+                data[key] = value;
+            }
+
+            return data;
+        }
+
+        return new Dictionary<string, string>();
     }
 }
