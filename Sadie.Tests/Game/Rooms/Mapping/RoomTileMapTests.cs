4f77f6ed5a9103f1bca77600d286136cc777ce29
habtard <habtard@gmail.com>
2024-08-21 14:09:49 +0100

    Test stuff

---
+++ Sadie.Tests/Game/Rooms/Mapping/RoomTileMapTests.cs
@@ -0,0 +1,40 @@
+namespace Sadie.Tests.Game.Rooms.Mapping;
+
+[TestFixture]
+public class RoomTileMapTests
+{
+    [Test]
+    public void UpdateEffectMapForTile_SwimTiles_WorksAsExpected()
+    {
+    }
+    
+    [Test]
+    public void AddUserToMap_AddUser_IncrementsMapCount()
+    {
+    }
+    
+    [Test]
+    public void AddBotToMap_AddBot_IncrementsMapCount()
+    {
+    }
+    
+    [Test]
+    public void UsersAtPoint_PopulatedPoint_ReturnsTrue()
+    {
+    }
+    
+    [Test]
+    public void UsersAtPoint_EmptyPoint_ReturnsFalse()
+    {
+    }
+    
+    [Test]
+    public void TileExists_NonExistentTile_ReturnsFalse()
+    {
+    }
+    
+    [Test]
+    public void TileExists_ExistingTile_ReturnsTrue()
+    {
+    }
+}
