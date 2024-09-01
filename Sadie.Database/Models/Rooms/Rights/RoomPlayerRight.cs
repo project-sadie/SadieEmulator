a69a9d765efc78c5bcdf6ce362f71b83c97b6308
habtard <habtard@gmail.com>
2024-09-01 01:03:09 +0100

    Entity update

--- Sadie.Database/Models/Rooms/Rights/RoomPlayerRight.cs
+++ Sadie.Database/Models/Rooms/Rights/RoomPlayerRight.cs
@@ -5,4 +5,5 @@ public class RoomPlayerRight
     public int Id { get; init; }
     public int RoomId { get; init; }
     public int PlayerId { get; init; }
+    public required DateTime CreatedAt { get; init; }
 }
