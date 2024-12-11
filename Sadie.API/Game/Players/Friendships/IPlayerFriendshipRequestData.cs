1e74909b9d91657ebf97c0f0087a7592aa6775c4
habtard <habtard@gmail.com>
2024-12-11 18:17:12 +0000

    Added packet data attributes

--- Sadie.API/Game/Players/Friendships/IPlayerFriendshipRequestData.cs
+++ Sadie.API/Game/Players/Friendships/IPlayerFriendshipRequestData.cs
@@ -1,8 +1,10 @@
+using Sadie.Shared.Attributes;
+
 namespace Sadie.API.Game.Players.Friendships;
 
 public interface IPlayerFriendshipRequestData
 {
-    long Id { get; init; }
-    string Username { get; init; }
-    string FigureCode { get; init; }
+    [PacketData] long Id { get; init; }
+    [PacketData] string Username { get; init; }
+    [PacketData] string FigureCode { get; init; }
 }
