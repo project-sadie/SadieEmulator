9eaf8a448cdb6ae1ddc0908dd079bf6aa75df52d
habtard <habtard@gmail.com>
2024-12-11 18:17:03 +0000

    Dropped import

--- Sadie.Networking/Client/NetworkClient.cs
+++ Sadie.Networking/Client/NetworkClient.cs
@@ -5,7 +5,6 @@ using Sadie.API.Game.Players;
 using Sadie.API.Game.Rooms.Users;
 using Sadie.API.Networking;
 using Sadie.Networking.Codecs.Encryption;
-using Sadie.Networking.Packets;
 using Sadie.Networking.Serialization;
 using Sadie.Options.Options;
 
