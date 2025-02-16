764ee362d7e7452298d5df65e4110f783e82c801
habtard <habtard@gmail.com>
2025-02-16 13:57:04 +0000

    fix for mixing writers

--- Sadie.Game.Rooms/Packets/Writers/Furniture/WiredTriggerWriter.cs
+++ Sadie.Game.Rooms/Packets/Writers/Furniture/WiredTriggerWriter.cs
@@ -1,4 +1,5 @@
 using Sadie.API.Networking;
+using Sadie.Enums.Game.Rooms.Furniture;
 using Sadie.Networking.Serialization.Attributes;
 using Sadie.Networking.Writers;
 
@@ -10,11 +11,23 @@ public class WiredTriggerWriter : AbstractPacketWriter
     public required bool StuffTypeSelectionEnabled { get; init; }
     public required int MaxItemsSelected { get; init; }
     public required List<int> SelectedItemIds { get; init; }
-    public required int AssetId { get; init; }
+    public required int StuffTypeId { get; init; }
     public required int Id { get; init; }
     public string Input { get; init; } = "";
     public required List<int> IntParameters { get; init; }
     public required int StuffTypeSelectionCode { get; init; }
     public required int TriggerConfig { get; init; }
     public required List<int> ConflictingEffectIds { get; init; }
+
+    public override void OnConfigureRules()
+    {
+        if (TriggerConfig == (int) WiredTriggerCode.AvatarSaysSomething && 
+            IntParameters[0] == 0)
+        {
+            Override(GetType().GetProperty(nameof(IntParameters))!, writer =>
+            { 
+                writer.WriteInteger(0);
+            });
+        }
+    }
 }
