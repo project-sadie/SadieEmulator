755dd22942c593c94a0b2c28e9a2740665a4a90e
habtard <habtard@gmail.com>
2025-02-25 19:37:39 +0000

    on place wired creation for effects

--- Sadie.Game.Rooms/Furniture/Interactors/Wired/Effects/WiredEffectGenericInteractor.cs
+++ Sadie.Game.Rooms/Furniture/Interactors/Wired/Effects/WiredEffectGenericInteractor.cs
@@ -1,7 +1,9 @@
+using Microsoft.EntityFrameworkCore;
 using Sadie.API.Game.Rooms;
 using Sadie.API.Game.Rooms.Furniture;
 using Sadie.API.Game.Rooms.Services;
 using Sadie.API.Game.Rooms.Users;
+using Sadie.Database;
 using Sadie.Database.Models.Constants;
 using Sadie.Database.Models.Players.Furniture;
 using Sadie.Enums.Game.Furniture;
@@ -9,12 +11,16 @@ using Sadie.Game.Rooms.Packets.Writers.Furniture;
 
 namespace Sadie.Game.Rooms.Furniture.Interactors.Wired.Effects;
 
-public class WiredEffectGenericInteractor(IRoomWiredService wiredService, ServerRoomConstants roomConstants) : AbstractRoomFurnitureItemInteractor
+public class WiredEffectGenericInteractor(
+    IRoomWiredService wiredService, 
+    ServerRoomConstants roomConstants,
+    SadieContext dbContext) : AbstractRoomFurnitureItemInteractor
 {
     public override List<string> InteractionTypes => [
         FurnitureItemInteractionType.WiredEffectKickUser,
         FurnitureItemInteractionType.WiredEffectShowMessage,
         FurnitureItemInteractionType.WiredEffectTeleportToFurniture,
+        FurnitureItemInteractionType.WiredEffectToggleFurnitureState,
     ];
 
     public override async Task OnTriggerAsync(IRoomLogic room, PlayerFurnitureItemPlacementData item, IRoomUser roomUser)
@@ -46,5 +52,28 @@ public class WiredEffectGenericInteractor(IRoomWiredService wiredService, Server
             DelayInPulses = 0,
             ConflictingTriggerIds = []
         });
+    }
+
+    public override async Task OnPlaceAsync(IRoomLogic room, PlayerFurnitureItemPlacementData item, IRoomUser roomUser)
+    {
+        if (item.WiredData != null)
+        {
+            return;
+        }
+
+        var wiredData = new PlayerFurnitureItemWiredData
+        {
+            PlayerFurnitureItemPlacementDataId = item.Id,
+            PlacementData = item,
+            Message = null,
+            PlayerFurnitureItemWiredParameters = [],
+            PlayerFurnitureItemWiredItems = []
+        };
+        
+        dbContext.Entry(wiredData).State = EntityState.Added;
+        dbContext.Entry(wiredData.PlacementData).State = EntityState.Unchanged;
+        await dbContext.SaveChangesAsync();
+
+        item.WiredData = wiredData;
     }
 }
