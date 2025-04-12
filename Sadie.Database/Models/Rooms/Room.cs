1e849318b90cb7fad926b4362bef0b4f6c169f1c
habtard <habtard@gmail.com>
2025-04-12 19:42:34 +0100

    removed lazy loader

--- Sadie.Database/Models/Rooms/Room.cs
+++ Sadie.Database/Models/Rooms/Room.cs
@@ -8,9 +8,7 @@ namespace Sadie.Database.Models.Rooms;
 
 public class Room : IRoom
 {
-    private readonly ILazyLoader _lazyLoader;
     private Player? _owner;
-    private RoomLayout? _layout;
     private RoomPaintSettings? _paintSettings;
     private RoomChatSettings? _chatSettings;
     private ICollection<RoomPlayerRight> _playerRights = [];
@@ -23,55 +21,20 @@ public class Room : IRoom
     public int Id { get; init; }
     public required string Name { get; set; }
     public int LayoutId { get; set; }
-    
-    public RoomLayout? Layout
-    {
-        get => _lazyLoader.Load(this, ref _layout);
-        set => _layout = value;
-    }
-    
+    public RoomLayout Layout { get; set; }
     public long OwnerId { get; init; }
-    
-    public Player? Owner
-    {
-        get => _lazyLoader.Load(this, ref _owner);
-        set => _owner = value;
-    }
-    
+    public Player? Owner { get; set; }
     public int MaxUsersAllowed { get; set; }
     public required string Description { get; set; }
     public bool IsMuted { get; init; }
-    
     public RoomSettings Settings { get; set; }
-    
-    public RoomPaintSettings? PaintSettings
-    {
-        get => _lazyLoader.Load(this, ref _paintSettings);
-        set => _paintSettings = value;
-    }
-    
-    public RoomChatSettings? ChatSettings
-    {
-        get => _lazyLoader.Load(this, ref _chatSettings);
-        set => _chatSettings = value;
-    }
-    
-    public ICollection<RoomPlayerRight> PlayerRights
-    {
-        get => _lazyLoader.Load(this, ref _playerRights);
-        init => _playerRights = value;
-    }
-    
+    public RoomPaintSettings PaintSettings { get; set; }
+    public RoomChatSettings? ChatSettings{ get; set; }
+    public ICollection<RoomPlayerRight> PlayerRights { get; init; }
     public ICollection<RoomChatMessage> ChatMessages { get; init; } = [];
     public ICollection<RoomTag> Tags { get; init; } = [];
     public ICollection<PlayerRoomLike> PlayerLikes { get; init; } = [];
-    
-    public ICollection<PlayerFurnitureItemPlacementData> FurnitureItems
-    {
-        get => _lazyLoader.Load(this, ref _furnitureItems);
-        init => _furnitureItems = value;
-    }
-    
+    public ICollection<PlayerFurnitureItemPlacementData> FurnitureItems{ get; init; }
     public DateTime CreatedAt { get; init; }
     public Group? Group { get; init; }
     public RoomDimmerSettings? DimmerSettings { get; set; }
