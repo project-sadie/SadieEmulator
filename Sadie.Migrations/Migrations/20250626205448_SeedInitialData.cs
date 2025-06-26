12e02ebebbb2a7e4fd17f50bb1301fa62bd97107
habtard <habtard@gmail.com>
2025-06-26 22:00:50 +0100

    migration files

---
+++ Sadie.Migrations/Migrations/20250626205448_SeedInitialData.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Sadie.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedInitialData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO server_settings (player_welcome_message, fair_currency_rewards) VALUES ('Welcome (back) to Sadie [username], we''re running version [version]!', 1)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("TRUNCATE TABLE server_settings;");
+        }
+    }
+}
