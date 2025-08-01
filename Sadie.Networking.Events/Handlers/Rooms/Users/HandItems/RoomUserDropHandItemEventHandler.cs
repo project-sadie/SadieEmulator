d003d6288804c27b38beced03c90198209827986
habtard <habtard@gmail.com>
2025-08-02 00:12:18 +0100

    bumped packages

--- Sadie.Networking.Events/Handlers/Rooms/Users/HandItems/RoomUserDropHandItemEventHandler.cs
+++ Sadie.Networking.Events/Handlers/Rooms/Users/HandItems/RoomUserDropHandItemEventHandler.cs
@@ -1,7 +1,7 @@
 using Sadie.API.Game.Rooms;
 using Sadie.Networking.Client;
-using Sadie.Shared.Attributes;
 using Sadie.Networking.Writers.Rooms.Users.HandItems;
+using Sadie.Shared.Attributes;
 
 namespace Sadie.Networking.Events.Handlers.Rooms.Users.HandItems;
 
