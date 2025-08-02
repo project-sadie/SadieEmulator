7e28d920da0c55fa2ca6db3b76b7789d559ec905
habtard <habtard@gmail.com>
2025-08-02 01:11:11 +0100

    ServiceCollectionHelpers

---
+++ Sadie.Server/ServiceCollectionHelpers.cs
@@ -0,0 +1,57 @@
+using System.Reflection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Sadie.API.Game.Rooms.Chat.Commands;
+using Sadie.API.Game.Rooms.Furniture;
+using Sadie.API.Game.Rooms.Furniture.Processors;
+using Serilog;
+
+namespace SadieEmulator;
+
+public static class ServiceCollectionHelpers
+{
+    public static void RegisterRoomChatCommands(this IServiceCollection serviceCollection, Assembly[]  assemblies)
+    {
+        serviceCollection.Scan(scan => scan
+            .FromAssemblies(assemblies)
+            .AddClasses(classes => classes.AssignableTo<IRoomChatCommand>())
+            .As<IRoomChatCommand>()
+            .WithSingletonLifetime());
+    }
+    
+    public static void RegisterFurnitureInteractors(this IServiceCollection serviceCollection, Assembly[]  assemblies)
+    {
+        serviceCollection.Scan(scan => scan
+            .FromAssemblies(assemblies)
+            .AddClasses(classes => classes.AssignableTo<AbstractRoomFurnitureItemInteractor>())
+            .AsImplementedInterfaces()
+            .WithSingletonLifetime());
+    }
+    
+    public static void RegisterRoomFurnitureProcessors(this IServiceCollection serviceCollection, Assembly[]  assemblies)
+    {
+        serviceCollection.Scan(scan => scan
+            .FromAssemblies(assemblies)
+            .AddClasses(classes => classes.AssignableTo<IRoomFurnitureItemProcessor>())
+            .AsImplementedInterfaces()
+            .WithSingletonLifetime());
+    }
+
+    public static void LoadPlugins(IConfiguration config)
+    {
+        var pluginFolder = config.GetValue<string>("PluginDirectory");
+
+        if (string.IsNullOrEmpty(pluginFolder) || !Directory.Exists(pluginFolder))
+        {
+            return;
+        }
+        
+        foreach (var plugin in Directory.GetFiles(pluginFolder, "*.dll", SearchOption.AllDirectories))
+        {
+            var assembly = Assembly.LoadFile(plugin);
+            var version = assembly.GetName().Version;
+            
+            Log.Logger.Warning($"Loaded plugin: {Path.GetFileNameWithoutExtension(plugin)} {version}");
+        }
+    }
+}
