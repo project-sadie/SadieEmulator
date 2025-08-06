c09b8ab9bf040c19274086e0160e6b99eef83b17
habtard <habtard@gmail.com>
2025-08-06 12:59:12 +0100

    fixes

--- Sadie.Networking.Events/Handlers/Rooms/Bots/RoomPlayerBotPlacedEventHandler.cs
+++ Sadie.Networking.Events/Handlers/Rooms/Bots/RoomPlayerBotPlacedEventHandler.cs
@@ -14,7 +14,7 @@ namespace Sadie.Networking.Events.Handlers.Rooms.Bots;
 
 [PacketId(EventHandlerId.RoomPlayerBotPlaced)]
 public class RoomPlayerBotPlacedEventHandler(
-    IDbContextFactory<SadieContext> dbContextFactory,
+    IDbContextFactory<SadieDbContext> dbContextFactory,
     IRoomRepository roomRepository,
     IRoomBotFactory roomBotFactory) : INetworkPacketEventHandler
 {
