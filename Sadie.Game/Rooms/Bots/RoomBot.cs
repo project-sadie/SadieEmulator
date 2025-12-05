ab6bc87ec97f5da92740025de9cf1278afedf331
habtard <habtard@gmail.com>
2025-12-05 16:23:09 +0000

    api stuff

--- Sadie.Game/Rooms/Bots/RoomBot.cs
+++ Sadie.Game/Rooms/Bots/RoomBot.cs
@@ -1,5 +1,5 @@
 using System.Drawing;
-using Sadie.API.DTOs.Player;
+using Sadie.API.DTOs.Players;
 using Sadie.API.Interfaces.Game.Rooms;
 using Sadie.API.Interfaces.Game.Rooms.Bots;
 using Sadie.API.Interfaces.Game.Rooms.Mapping;
--- Sadie.Game/Rooms/Furniture/RoomFurnitureItemHelperService.cs
+++ Sadie.Game/Rooms/Furniture/RoomFurnitureItemHelperService.cs
@@ -1,6 +1,6 @@
 using AutoMapper;
 using Microsoft.EntityFrameworkCore;
-using Sadie.API.DTOs.Player.Furniture;
+using Sadie.API.DTOs.Players.Furniture;
 using Sadie.API.Interfaces.Game.Players;
 using Sadie.API.Interfaces.Game.Rooms;
 using Sadie.API.Interfaces.Game.Rooms.Furniture;
