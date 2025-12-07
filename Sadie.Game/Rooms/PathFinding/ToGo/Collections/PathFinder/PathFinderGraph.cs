16f4147129b47f848953fb9af2a5b9c8c30505cd
habtard <habtard@gmail.com>
2025-12-07 00:07:23 +0000

    improvements to pathfinding

--- Sadie.Game/Rooms/PathFinding/ToGo/Collections/PathFinder/PathFinderGraph.cs
+++ Sadie.Game/Rooms/PathFinding/ToGo/Collections/PathFinder/PathFinderGraph.cs
@@ -8,7 +8,6 @@ internal class PathFinderGraph : IModelAGraph<PathFinderNode>
     private readonly bool _allowDiag;
     private readonly Grid<PathFinderNode> _grid;
     private readonly bool[,] _visited;
-
     private readonly PathFinderNode[] _heap;
     private int _count;
 
@@ -19,10 +18,7 @@ internal class PathFinderGraph : IModelAGraph<PathFinderNode>
         _allowDiag = allowDiag;
         _grid = new Grid<PathFinderNode>(height, width);
         _visited = new bool[height, width];
-
-        // Priority queue capacity = all nodes max
         _heap = new PathFinderNode[height * width];
-
         Reset();
     }
 
@@ -37,16 +33,71 @@ internal class PathFinderGraph : IModelAGraph<PathFinderNode>
                 _visited[r, c] = false;
             }
         }
-
         _count = 0;
+    }
+
+    private readonly struct SuccessorEnumerable(Grid<PathFinderNode> g, IPosition p, bool diag)
+        : IEnumerable<PathFinderNode>
+    {
+        IEnumerator<PathFinderNode> IEnumerable<PathFinderNode>.GetEnumerator()
+        {
+            return new SuccessorEnumerator(g, p, diag);
+        }
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            return new SuccessorEnumerator(g, p, diag);
+        }
+    }
+
+    private struct SuccessorEnumerator(Grid<PathFinderNode> g, IPosition p, bool diag) : IEnumerator<PathFinderNode>
+    {
+        private int _i = -1;
+        private PathFinderNode _current = default;
+
+        private static readonly (int r, int c)[] Offsets =
+        {
+            (-1,0),(1,0),(0,-1),(0,1),
+            (-1,-1),(-1,1),(1,-1),(1,1)
+        };
+
+        public bool MoveNext()
+        {
+            while (true)
+            {
+                _i++;
+                if (_i >= 8)
+                {
+                    return false;
+                }
+                if (_i >= 4 && !diag)
+                {
+                    continue;
+                }
+
+                var (dr, dc) = Offsets[_i];
+                var nr = p.Row + dr;
+                var nc = p.Column + dc;
+
+                if ((uint)nr >= (uint)g.Height || (uint)nc >= (uint)g.Width)
+                {
+                    continue;
+                }
+
+                _current = g[nr, nc];
+                return true;
+            }
+        }
+
+        public PathFinderNode Current => _current;
+        object System.Collections.IEnumerator.Current => _current;
+        public void Reset() { _i = -1; }
+        public void Dispose() { }
     }
 
     public IEnumerable<PathFinderNode> GetSuccessors(PathFinderNode n)
     {
-        foreach (var pos in _grid.GetSuccessorPositions(n.Position, _allowDiag))
-        {
-            yield return _grid[pos];
-        }
+        return new SuccessorEnumerable(_grid, n.Position, _allowDiag);
     }
 
     public PathFinderNode GetParent(PathFinderNode n)
@@ -64,16 +115,18 @@ internal class PathFinderGraph : IModelAGraph<PathFinderNode>
         _visited[n.Position.Row, n.Position.Column] = true;
         _grid[n.Position] = n;
 
-        // push to heap
         var i = _count++;
         _heap[i] = n;
 
         while (i > 0)
         {
             var parent = (i - 1) >> 1;
-            if (_heap[i].F >= _heap[parent].F) break;
+            if (_heap[i].F >= _heap[parent].F)
+            {
+                break;
+            }
 
-            ( _heap[i], _heap[parent] ) = ( _heap[parent], _heap[i] );
+            (_heap[i], _heap[parent]) = (_heap[parent], _heap[i]);
             i = parent;
         }
     }
@@ -86,22 +139,30 @@ internal class PathFinderGraph : IModelAGraph<PathFinderNode>
         if (_count > 0)
         {
             _heap[0] = _heap[_count];
-
             var i = 0;
+
             while (true)
             {
                 var left = (i << 1) + 1;
-                if (left >= _count) break;
+                if (left >= _count)
+                {
+                    break;
+                }
 
                 var right = left + 1;
                 var best = left;
 
                 if (right < _count && _heap[right].F < _heap[left].F)
+                {
                     best = right;
+                }
 
-                if (_heap[i].F <= _heap[best].F) break;
+                if (_heap[i].F <= _heap[best].F)
+                {
+                    break;
+                }
 
-                ( _heap[i], _heap[best] ) = ( _heap[best], _heap[i] );
+                (_heap[i], _heap[best]) = (_heap[best], _heap[i]);
                 i = best;
             }
         }
