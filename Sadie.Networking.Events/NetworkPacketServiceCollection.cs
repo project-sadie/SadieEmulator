b257b93c8d49a534a2ec67886c61e346de094d71
habtard <habtard@gmail.com>
2026-04-15 01:23:07 +0100

    better throughput on packets

--- Sadie.Networking.Events/NetworkPacketServiceCollection.cs
+++ Sadie.Networking.Events/NetworkPacketServiceCollection.cs
@@ -45,6 +45,6 @@ public static class NetworkPacketServiceCollection
         serviceCollection.AddSingleton<INetworkClientConnectionHandler, NetworkClientConnectionHandler>();
         serviceCollection.AddSingleton<PacketDispatcher>(p => new PacketDispatcher(
             p.GetRequiredService<INetworkPacketHandler>(),
-            Environment.ProcessorCount / 2));
+            Environment.ProcessorCount * 2));
     }
 }
